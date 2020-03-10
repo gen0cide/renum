92309cba6bc8767c49986cefdadcc8c349280a0c
Alex <gen0cide.threats@gmail.com>
2020-03-10 13:04:08 -0700

    bumping version with change

--- version.go
+++ version.go
@@ -2,10 +2,10 @@ package renum
 
 var (
 	// Version describes the version of the library.
-	Version = `0.0.8`
+	Version = `0.0.9`
 
 	// Build describes the git revision for this build.
-	Build = `970c1e6`
+	Build = `f7a19b7`
 )
 
 // VersionString returns the renum library version, using semantic versioning (https://semver.org)
